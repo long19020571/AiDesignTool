c2fd0fd2c3e431fab4d4f862297cc21bc163a0a4
long19020571 <passionforhighspeed@gmail.com>
2024-10-09 11:56:31 +0700

    Init

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AiDesignTool
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
